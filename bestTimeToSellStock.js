ed315382fc129cae0155c2c39302d8e477398147
JBayona <jordavids_22@hotmail.com>
2018-10-10 23:25:48 -0700

    Best Time To Sell Stock.

--- bestTimeToSellStock.js
+++ bestTimeToSellStock.js
@@ -66,3 +66,31 @@ var maxProfit = function(prices) {
     }
     return maxSoFar;
 };
+
+/*
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. You may complete as many transactions as you
+like (i.e., buy one and sell one share of the stock multiple times).
+
+Note: You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
+
+Example 1:
+
+Input: [7,1,5,3,6,4]
+Output: 7
+Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+             Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/description/
+
+*/
+
+var maxProfit = function(prices) {
+    let result = 0;
+    for(let i = 1; i < prices.length; i++) {
+        // Math with 0 to reset in case it's a negative number
+        result += Math.max(0, prices[i] - prices[i-1]);
+    }
+    return result;
+};
