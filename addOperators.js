02fbfaefc3cf495f24932b503cd5681ef202d0bc
JBayona <jordavids_22@hotmail.com>
2018-12-09 16:37:07 -0800

    Add Operator expression.

--- addOperators.js
+++ addOperators.js
@@ -1,5 +1,6 @@
 /*
-Given a string that contains only digits 0-9 and a target value, return all possibilities to add binary operators (not unary) +, -, or *between the digits so they evaluate to the target value.
+Given a string that contains only digits 0-9 and a target value, return all possibilities to add binary operators (not unary) +, -, or *
+between the digits so they evaluate to the target value.
 
 Example 1:
 
