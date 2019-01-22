af2a1447f9ac9076ad0a914766af1671389df9ea
JBayona <jordavids_22@hotmail.com>
2019-01-21 22:03:54 -0800

    Decode ways.

--- decodeWays.js
+++ decodeWays.js
@@ -19,6 +19,7 @@ Output: 3
 Explanation: It could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
 
 https://youtu.be/qli-JCrSwuk
+https://www.youtube.com/watch?v=YcJTyrG3bZs
 
 https://leetcode.com/problems/decode-ways/description/
 */
